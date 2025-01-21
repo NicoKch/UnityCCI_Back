c7a4b149354b0edd2311b7fa6ac4ebd148e7fb56
Nicolas Kirchhoffer <nkirchhoffer@gmail.com>
2025-01-21 23:57:02 +0100

    add readme

---
+++ database/InsertScript.sql
@@ -0,0 +1,28 @@
+INSERT INTO `users`
+(
+`mail`,
+`password`,
+`userName`)
+VALUES
+(
+"mail@test.com",
+"azerty",
+"darkSasuke");
+
+
+INSERT INTO `stats`
+(
+`level`,
+`deathcount`,
+`playtime`,
+`rank`,
+`power`,
+`user_id`)
+VALUES
+(
+143,
+21,
+67,
+892,
+1472,
+1);
